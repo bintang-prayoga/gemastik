e2a7f3228591c406e02ee90e21e8906298cb2725
Bintang <bprayoga1927@gmail.com>
2024-08-04 12:50:43 +0700

    first commit

---
+++ src/main/java/org/yourcompany/gemastik/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package org.yourcompany.gemastik;
+
+/**
+ *
+ * @author BoedNoNiwa
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
